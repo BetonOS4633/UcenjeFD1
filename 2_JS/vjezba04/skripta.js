0c5bdd2092e95bf5564a028fe213bebf18b67057
gergelj <rudolf.gergelj@gmail.com>
2026-02-03 18:58:49 +0100

    vjezba js07

---
+++ 2_JS/vjezba04/skripta.js
@@ -0,0 +1,22 @@
+//
+
+
+
+const uvjet=true;
+if(uvjet){
+    console.log('Uvjet je true');
+}
+
+if(uvjet){
+    console.log('uvjet je true');
+}
+else (console.log('uvjet je false');
+'uvjet je false') 
+  const ocjena=3
+
+  if(ocjena===1){
+    console.log('nedovoljan');   
+  }
+  else{ if}
+
+
