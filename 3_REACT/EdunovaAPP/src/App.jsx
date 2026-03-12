d4f8dc95f8ed4b170c21ee03c5cdd261c8df23a0
gergelj <rudolf.gergelj@gmail.com>
2026-03-12 18:52:18 +0100

    riktanje edunova

--- 3_REACT/EdunovaAPP/src/App.jsx
+++ 3_REACT/EdunovaAPP/src/App.jsx
@@ -1,11 +1,17 @@
+import'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
+import { Container } from 'react-bootstrap'
+import Izbornik from './components/Izbornik'
+
 
 function App() {
 
-  return (
-    <>
-      Hello Edunova
-    </>
+  return ( 
+    <Container>
+      <Izbornik></Izbornik>
+
+
+    </Container>
   )
 }
 
